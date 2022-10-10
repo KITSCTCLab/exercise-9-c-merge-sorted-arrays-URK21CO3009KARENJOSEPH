e632182cfb3c7fcfff10fb1a8807b9e50d73c663
URK21CO3009KARENJOSEPH <110015429+URK21CO3009KARENJOSEPH@users.noreply.github.com>
2022-10-10 15:19:32 +0530

    Create Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,1 @@
-from typing import List
-
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  # Write code here
-
-
-# Do not change the following code
-nums1 = []
-nums2 = []
-for item in input().split(', '):
-  nums1.append(int(item))
-for item in input().split(', '):
-  nums2.append(int(item))
-m = int(input())
-n = int(input())
-merge(nums1, m, nums2, n)
-print(nums1)
+https://github.com/KITSCTCLab/exercise-9-b-quick-sort-URK21CO3009KARENJOSEPH/blob/main/Main.py
